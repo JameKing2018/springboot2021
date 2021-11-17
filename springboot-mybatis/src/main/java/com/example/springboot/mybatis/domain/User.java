364c9d3191253c7b5f52bc6b79678a20b5739906
king <610603255@qq.com>
2021-11-17 17:27:28 +0800

    use mybatis to control mysql

---
+++ springboot-mybatis/src/main/java/com/example/springboot/mybatis/domain/User.java
@@ -0,0 +1,14 @@
+package com.example.springboot.mybatis.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+}
---
+++ springboot-mybatis/src/main/java/com/example/springboot/mybatis/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.example.springboot.mybatis.mapper;
+
+import com.example.springboot.mybatis.domain.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Select("select * from user where name =#{name}")
+    User findByName(@Param("name") String name);
+    @Insert("insert into user(name,age) values(#{name},#{age})")
+    int insert(@Param("name") String name,@Param("age") Integer age);
+}
