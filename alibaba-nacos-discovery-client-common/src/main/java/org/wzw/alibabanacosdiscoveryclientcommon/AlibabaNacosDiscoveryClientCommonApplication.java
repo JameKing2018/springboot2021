bb982bb06b56ece777621bf0c07f09d3158db014
king <610603255@qq.com>
2021-11-19 11:48:31 +0800

    alibaba-nacos-discovery-client-common  use @loadblanced

--- alibaba-nacos-discovery-client-common/src/main/java/org/wzw/alibabanacosdiscoveryclientcommon/AlibabaNacosDiscoveryClientCommonApplication.java
+++ alibaba-nacos-discovery-client-common/src/main/java/org/wzw/alibabanacosdiscoveryclientcommon/AlibabaNacosDiscoveryClientCommonApplication.java
@@ -8,13 +8,17 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class AlibabaNacosDiscoveryClientCommonApplication {
 
     public static void main(String[] args) {
@@ -31,9 +35,14 @@ public class AlibabaNacosDiscoveryClientCommonApplication {
         @Autowired
         RestTemplate restTemplate;
 
+        @Autowired
+        Client client;
+
+
         @GetMapping("/test")
         public String test(){
-            String result=restTemplate.getForObject("http://alibaba-nacos-discovery-server/hello?name=snow51",String.class);
+
+            String result=client.hello(" snow51");
             return result;
         }
 
@@ -41,6 +50,11 @@ public class AlibabaNacosDiscoveryClientCommonApplication {
         @LoadBalanced
         public RestTemplate restTemplate(){
             return  new RestTemplate();
+        }
+        @FeignClient("alibaba-nacos-discovery-server")
+        interface Client{
+            @GetMapping("/hello")
+            String hello(@RequestParam(name="name") String name);
         }
     }
 
