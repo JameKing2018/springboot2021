17dd19e078951f36affeff5921aa58f88e4abf66
king <610603255@qq.com>
2021-11-19 11:41:36 +0800

    alibaba-nacos-discovery-client-common  use @loadblanced

--- alibaba-nacos-discovery-client-common/src/main/java/org/wzw/alibabanacosdiscoveryclientcommon/AlibabaNacosDiscoveryClientCommonApplication.java
+++ alibaba-nacos-discovery-client-common/src/main/java/org/wzw/alibabanacosdiscoveryclientcommon/AlibabaNacosDiscoveryClientCommonApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -26,13 +28,19 @@ public class AlibabaNacosDiscoveryClientCommonApplication {
         @Autowired
         LoadBalancerClient loadBalancerClient;
 
+        @Autowired
+        RestTemplate restTemplate;
+
         @GetMapping("/test")
         public String test(){
-            ServiceInstance serviceInstance=loadBalancerClient.choose("alibaba-nacos-discovery-server");
-            String url=serviceInstance.getUri()+"/hello?name="+"snow51";
-            RestTemplate restTemplate=new RestTemplate();
-            String result=restTemplate.getForObject(url,String.class);
+            String result=restTemplate.getForObject("http://alibaba-nacos-discovery-server/hello?name=snow51",String.class);
             return result;
+        }
+
+        @Bean
+        @LoadBalanced
+        public RestTemplate restTemplate(){
+            return  new RestTemplate();
         }
     }
 
